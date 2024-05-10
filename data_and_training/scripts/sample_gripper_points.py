4cc145e501711c475781c6191276557238c7963f
Jiacheng Yuan <yuanx320@umn.edu>
2024-05-09 23:37:31 -0700

    Generate Robotiq-2f-85 mesh in grasp frame

--- data_and_training/scripts/sample_gripper_points.py
+++ data_and_training/scripts/sample_gripper_points.py
@@ -28,29 +28,46 @@ def pcd_to_spheres(pcd):
     return spheres
 
 frame_base = draw_frame(np.zeros(3), np.array([0, 0, 0, 1]), scale=0.1)
+# gripper_mesh = o3d.io.read_triangle_mesh(
+#                 "resources/SR_Gripper_Collision_Open.stl")
 gripper_mesh = o3d.io.read_triangle_mesh(
-                "resources/SR_Gripper_Collision_Open.stl")
+                "resources/2F85_opened.STL")
+gripper_mesh.scale(0.001, center=np.zeros(3))
+z_offset = (gripper_mesh.get_max_bound()[2] + gripper_mesh.get_min_bound()[2]) / 2
+gripper_mesh.translate(np.array([0, 0, -z_offset]))
+gripper_mesh.rotate(np.array([[1, 0, 0],
+                              [0, 0, -1],
+                              [0, 1, 0]]),
+                    center=np.zeros(3))
+gripper_mesh.rotate(np.array([[0, -1, 0],
+                              [1, 0, 0],
+                              [0, 0, 1]]),
+                    center=np.zeros(3))
+gripper_mesh.translate(np.array([0, 0, -0.18]))
+gripper_mesh.compute_vertex_normals()
+o3d.io.write_triangle_mesh("resources/2f85_open_grasp_frame.stl", gripper_mesh)
+o3d.visualization.draw_geometries([gripper_mesh, frame_base])
 
-# Translate to the grasp pose frame in SceneGrasp convention
-gripper_mesh.translate(np.array([0,0,-0.21]))
+# # Translate to the grasp pose frame in SceneGrasp convention
+# # gripper_mesh.translate(np.array([0,0,-0.21]))
 
-pcd = gripper_mesh.sample_points_uniformly(number_of_points=1500)
+# pcd = gripper_mesh.sample_points_uniformly(number_of_points=1500)
 
-# Create bounding box:
-bounds = [[-math.inf, math.inf], [-math.inf, math.inf], [Z_MIN, math.inf]]  # set the bounds
-bounding_box_points = list(itertools.product(*bounds))  # create limit points
-bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
-    o3d.utility.Vector3dVector(bounding_box_points))  # create bounding box object
-# Crop the point cloud using the bounding box:
-pcd_cropped = pcd.crop(bounding_box)
+# # Create bounding box:
+# bounds = [[-math.inf, math.inf], [-math.inf, math.inf], [Z_MIN, math.inf]]  # set the bounds
+# bounding_box_points = list(itertools.product(*bounds))  # create limit points
+# bounding_box = o3d.geometry.AxisAlignedBoundingBox.create_from_points(
+#     o3d.utility.Vector3dVector(bounding_box_points))  # create bounding box object
+# # Crop the point cloud using the bounding box:
+# pcd_cropped = pcd.crop(bounding_box)
 
-# Downsample
-# pcd_cropped = pcd_cropped.voxel_down_sample(VOXEL_SIZE)
-pcd_cropped = pcd_cropped.farthest_point_down_sample(250)
+# # Downsample
+# # pcd_cropped = pcd_cropped.voxel_down_sample(VOXEL_SIZE)
+# pcd_cropped = pcd_cropped.farthest_point_down_sample(250)
 
-spheres = pcd_to_spheres(pcd_cropped)
-print("Number of spheres: %d"%(len(spheres)))
-o3d.visualization.draw_geometries(spheres + [gripper_mesh, frame_base])
+# spheres = pcd_to_spheres(pcd_cropped)
+# print("Number of spheres: %d"%(len(spheres)))
+# o3d.visualization.draw_geometries(spheres + [gripper_mesh, frame_base])
 
-pts_tensor = torch.from_numpy(np.asarray(pcd_cropped.points))
-torch.save(pts_tensor, 'resources/SR_Gripper_Collision_Open.pt')
+# pts_tensor = torch.from_numpy(np.asarray(pcd_cropped.points))
+# torch.save(pts_tensor, 'resources/SR_Gripper_Collision_Open.pt')
